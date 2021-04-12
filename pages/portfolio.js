88b4695b58a1752f693b263fcd00bccbed47c4b0
r3dreck <todd@consumedesign.com>
2021-04-12 08:53:30 -0400

    favicon add

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -100,7 +100,7 @@ class Portfolio extends Component {
                                     const content = post.content.rendered;
 
                                     if ('wp:featuredmedia' in post._embedded) {
-                                        console.log(post)
+                                        //  console.log(post)
                                         return (
                                             <li className={portfolioStyles.card} id={post.id} key={post.id} ref={ref[post.id]}>
 
