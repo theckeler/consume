a3ed7613cdf18294f5c673ab7343a7dccf3d68aa
r3dreck <todd@consumedesign.com>
2021-04-20 16:48:43 -0400

    v1.1

--- pages/index.js
+++ pages/index.js
@@ -32,47 +32,47 @@ function Blog({ posts }) {
 
       <Header currentPage="home" className={portfolioStyles.nav} />
 
-      <section className="section">
-        <h1 className="title">Featured Projects</h1>
-      </section>
+      <div className="main">
+        <section className="section">
+          <h1 className="title">Featured Projects</h1>
+        </section>
 
-      <section className={portfolioStyles.section}>
-        <div className={'wrapper ' + portfolioStyles.wrapper}>
+        <section className={portfolioStyles.section}>
+          <div className={'wrapper ' + portfolioStyles.wrapper}>
 
-          <ul className="grid noload">
-            {console.log(posts)}
-            {
-              posts.map(post => {
-                const date = new Date(post.date)
-                return (
-                  <li className={portfolioStyles.featured + ' ' + portfolioStyles.card} key={post.slug}>
-                    <a href={post.link} target="_blank" data-caption={post.title.rendered}>
-                      <strong>{post.title.rendered}</strong>
-                      <Image
-                        src={post._embedded['wp:featuredmedia']['0'].source_url}
-                        height={post._embedded["wp:featuredmedia"][0].media_details.height}
-                        width={post._embedded["wp:featuredmedia"][0].media_details.width}
-                        alt={post.title.rendered}
-                        layout="responsive"
-                      />
-                      <span className={portfolioStyles.extlink}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                          <path d="M5 3c-1.093 0-2 .907-2 2v14c0 1.093.907 2 2 2h14c1.093 0 2-.907 2-2v-7h-2v7H5V5h7V3H5zm9 0v2h3.586l-9.293 9.293 1.414 1.414L19 6.414V10h2V3h-7z" />
-                        </svg>
-                      </span>
-                      <div className={portfolioStyles.content}>
-                        <span dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-                        <span><p>Date Created: {date.getFullYear()}</p></span>
-                      </div>
-                    </a>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </section>
-
+            <ul className="grid noload">
+              {
+                posts.map(post => {
+                  const date = new Date(post.date)
+                  return (
+                    <li className={portfolioStyles.featured + ' ' + portfolioStyles.card} key={post.slug}>
+                      <a href={post.link} target="_blank" data-caption={post.title.rendered}>
+                        <strong>{post.title.rendered}</strong>
+                        <Image
+                          src={post._embedded['wp:featuredmedia']['0'].source_url}
+                          height={post._embedded["wp:featuredmedia"][0].media_details.height}
+                          width={post._embedded["wp:featuredmedia"][0].media_details.width}
+                          alt={post.title.rendered}
+                          layout="responsive"
+                        />
+                        <span className={portfolioStyles.extlink}>
+                          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <path d="M5 3c-1.093 0-2 .907-2 2v14c0 1.093.907 2 2 2h14c1.093 0 2-.907 2-2v-7h-2v7H5V5h7V3H5zm9 0v2h3.586l-9.293 9.293 1.414 1.414L19 6.414V10h2V3h-7z" />
+                          </svg>
+                        </span>
+                        <div className={portfolioStyles.content}>
+                          <span dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+                          <span><p>Date Created: {date.getFullYear()}</p></span>
+                        </div>
+                      </a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </section>
+      </div>
 
       <Footer currentPage="home" />
     </>
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -135,49 +135,50 @@ query MyQuery {
 
                 <Header currentPage="portfolio" className={portfolioStyles.nav} />
 
-                <section className="section">
-                    <h1 className="title">portfolio</h1>
-                </section>
+                <div className="main">
+                    <section className="section">
+                        <h1 className="title">portfolio</h1>
+                    </section>
 
-                <section className={portfolioStyles.section}>
-                    <div id="portfolio-container" className={'load page loading wrapper '}>
+                    <section className={portfolioStyles.section}>
+                        <div id="portfolio-container" className={'load page loading wrapper '}>
 
-                        <ul className="grid" id="portfolio-main">
-                            {
-                                this.state.posts.map(post => {
-                                    const thisPost = post.node;
-                                    const date = new Date(thisPost.dateGmt)
-                                    //console.log(thisPost)
+                            <ul className="grid" id="portfolio-main">
+                                {
+                                    this.state.posts.map(post => {
+                                        const thisPost = post.node;
+                                        const date = new Date(thisPost.dateGmt)
+                                        //console.log(thisPost)
 
-                                    if (thisPost) {
-                                        return (
-                                            <li
-                                                className={portfolioStyles.card + ' card loading'}
-                                                key={thisPost.id}
-                                            >
-                                                <a onClick={this.handleClick} href={thisPost.featuredImage.node.sourceUrl}>
-                                                    <strong dangerouslySetInnerHTML={{ __html: thisPost.title }} />
-                                                    <Image
-                                                        src={thisPost.featuredImage.node.sourceUrl}
-                                                        height={thisPost.featuredImage.node.mediaDetails.height}
-                                                        width={thisPost.featuredImage.node.mediaDetails.width}
-                                                        alt={thisPost.title}
-                                                    />
-                                                    <div className={portfolioStyles.content}>
-                                                        <span dangerouslySetInnerHTML={{ __html: thisPost.content }} />
-                                                        <span><p>Date Created: {date.getFullYear()}</p></span>
-                                                    </div>
-                                                </a>
-                                            </li>
-                                        )
-                                    }
-                                })
-                            }
-                            <div className="observer" ref={loadingRef => (this.loadingRef = loadingRef)} />
-                        </ul>
-                    </div>
-
-                </section>
+                                        if (thisPost) {
+                                            return (
+                                                <li
+                                                    className={portfolioStyles.card + ' card loading'}
+                                                    key={thisPost.id}
+                                                >
+                                                    <a onClick={this.handleClick} href={thisPost.featuredImage.node.sourceUrl}>
+                                                        <strong dangerouslySetInnerHTML={{ __html: thisPost.title }} />
+                                                        <Image
+                                                            src={thisPost.featuredImage.node.sourceUrl}
+                                                            height={thisPost.featuredImage.node.mediaDetails.height}
+                                                            width={thisPost.featuredImage.node.mediaDetails.width}
+                                                            alt={thisPost.title}
+                                                        />
+                                                        <div className={portfolioStyles.content}>
+                                                            <span dangerouslySetInnerHTML={{ __html: thisPost.content }} />
+                                                            <span><p>Date Created: {date.getFullYear()}</p></span>
+                                                        </div>
+                                                    </a>
+                                                </li>
+                                            )
+                                        }
+                                    })
+                                }
+                                <div className="observer" ref={loadingRef => (this.loadingRef = loadingRef)} />
+                            </ul>
+                        </div>
+                    </section>
+                </div>
 
                 <Footer currentPage="portfolio"></Footer>
             </>
