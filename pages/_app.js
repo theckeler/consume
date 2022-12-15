082afea711e2fdb9aa06b3f68d3e655f31c86701
Todd Heckeler <todd@consumedesign.com>
2022-12-15 09:30:49 -0500

    adding Audience Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
